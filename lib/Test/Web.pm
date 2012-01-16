358bcd43a487d46a6b6569da849ceeee81cec344
Kuniyoshi Kouji <kuniyoshi@cpan.org>
2012-01-17 00:49:30 +0900

    initial commit

---
+++ lib/Test/Web.pm
@@ -0,0 +1,32 @@
+package Test::Web;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Test::Web -
+
+=head1 SYNOPSIS
+
+  use Test::Web;
+
+=head1 DESCRIPTION
+
+Test::Web is
+
+=head1 AUTHOR
+
+kuniyoshi E<lt>kuniyoshi@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
